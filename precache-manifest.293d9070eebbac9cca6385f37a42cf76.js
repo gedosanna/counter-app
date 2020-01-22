ee284cc5e8a541d07df4065a77c0dbbfe7594c6f
gedosanna <56969611+gedosanna@users.noreply.github.com>
2020-01-22 19:56:56 +0100

    Updates

---
+++ precache-manifest.293d9070eebbac9cca6385f37a42cf76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ae860ef1d1f06171d1e59e9f3638e5",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "a3d87a2957017060289b",
+    "url": "/counter-app/static/css/main.d70d709d.chunk.css"
+  },
+  {
+    "revision": "6fda2421ee0e33c23086",
+    "url": "/counter-app/static/js/2.96b8afe6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter-app/static/js/2.96b8afe6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3d87a2957017060289b",
+    "url": "/counter-app/static/js/main.3bda9f9e.chunk.js"
+  },
+  {
+    "revision": "1cb6a0601cc0d8f5ab21",
+    "url": "/counter-app/static/js/runtime-main.06758c85.js"
+  }
+]);
